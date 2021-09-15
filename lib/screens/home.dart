3177c2c76e1d63b6c3fe6d7e8e499e79ef1d2c76
muhlikVision <soleh5617@gmail.com>
2021-09-15 16:29:57 +0500

    15sep2021

---
+++ lib/screens/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+
+class HomeScreen extends StatefulWidget {
+
+  static const String id = 'home_screen';
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
